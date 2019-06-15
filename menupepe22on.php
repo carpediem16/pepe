ea7660ab3b5b050e7074addfcb7b0cd76a20ebff
carpediem16 <40045806+carpediem16@users.noreply.github.com>
2019-06-15 06:37:00 -0500

    Add files via upload
    menu

---
+++ menupepe22on.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Menu Pepe</title>
+</head>
+<style>
+  body {
+    margin:9px 9px 0 9x;
+    padding:0;}
+  #level0 {
+    background:#FC0;
+    width:100%;}
+  #level1 {
+    margin-left:143px;
+    padding-left:9px;
+    background:#FFF;}
+  #level2 {
+    background:#FFF3AC;
+    position:relative;
+    width:inherit;}
+  #main {
+    margin-right:9em;
+    padding-right:9px;
+    background:#FFF;}
+  #topBar{
+    background:#FC0;}
+  #advBar{
+    background:#FFF3AC;}
+  #tipDay{
+    float:right;
+    width:175px;
+    background:#FFF3AC;
+    position:relative;}
+  #lftBar {
+    position:absolute;
+    width:143px;
+    top:9px;
+    left:9px;}
+  #rgtBar {
+    position:absolute;
+    width:9em;
+    top:0;
+    right:0;}
+  #main h1 {
+    margin:0;
+    padding-left:.3em;
+    font:1.25em Verdana,Helvetica,Arial;
+    color:#609;
+    background:#FC0;}
+  #main dt {
+    font-weight:bold;
+    font-size:120%;
+    margin-top:.8em}
+  #rgtBar h3 {
+    margin:0;
+    padding:3px;
+    background:#FC0;
+    font-weight:bold;
+    font-size:1em;
+    text-align:center;}
+  #topBar form {
+    float:right;
+    width:10.5em;
+    text-align:right;}
+</style>
+...
+<body>
+  <div id="lftBar">
+<img src="scripts/sonrie.jpg" width="59" height="57"><br />
+<br>
+<a href="http://twitter.com/#search?q=peru" target ="_blank">Twitter Peru</a><br />
+  </div>
+  <div id="level0">
+    <div id="level1">
+      <div id="topBar">
+        <form method=get action="">
+          <input type="text" name="query" size="12">
+          <input type="submit" value="Buscar">
+        </form>
+        Una sonrisa no cuesta nada. El cliente siempre tiene la razon.
+        <div id="advBar">
+<!..<img src="scripts/logobox.jpg" width="840" height="200"><br />
+        </div>
+      </div>
+      <div id="level2">
+<br>
+<a href="http://twitter.com/#search?q=peru" target ="_blank">Twitter Peru</a><br />
+<br>
+      <div id="rgtBar">
+        <h3>Webs Recomendadas</h3>
+<a href="http://caminante16.epizy.com/menupepe22on.php">Inicio de Sistema</a><br />
+<br />
+<a href="http://twitter.com/#search?q=peru" target ="_blank">Twitter Peru</a><br />
+<br />
+<img src="scripts/ayuda.GIF" width="145" height="120"><br />
+      
+        
+        <h3>Expertos</h3>
+<a href="http://twitter.com/#search?q=peru" target ="_blank">Twitter Peru</a><br />
+      </div>
+        <div id="main">
+
+
+<h1><div align="center">Menu del sistema de Hostal</div></h1>
+<a href="http://caminante16.epizy.com/saldo_totalpepe4on.php">Saldo Total de todos los negocios</a>
+<br>
+<a href="http://caminante16.epizy.com/saldo_totalpepe5on.php">Saldo Total de todos los negocios del mes </a>
+<br>
+<a href="http://caminante16.epizy.com/totalion.php">Saldo Total de ingresos de todos los negocios por codigo</a>
+<br>
+<a href="http://caminante16.epizy.com/total1on.php">Saldo Total de gastos de todos los negocios por codigo</a>
+<br>
+<a href="http://caminante16.epizy.com/form_insclion.php">agregar ingresos</a>
+<br>
+<a href="http://caminante16.epizy.com/form_insgastopepe.php">agregar Gastos </a>
+<br>
+<br>
+<a href="http://caminante16.epizy.com/menupepe22on2.php">agregar compras stock </a>
+<br>
+<a href="http://caminante16.epizy.com/menupepe22on1.php">agregar ventas stock </a>
+<br>
+<br>
+<a href="http://caminante16.epizy.com/tenlastingon.php">10 ultimos ingresos</a>
+<br>
+<a href="http://caminante16.epizy.com/tenlastegreon.php">10 ultimos gastos</a>
+<br>
+<a href="http://caminante16.epizy.com/cierraingppcon.php">cerrar mes ingresos</a>
+<br>
+<a href="http://caminante16.epizy.com/cierragasppcon.php">cerrar mes gastos</a>
+
+</h3>
+<br>
+<br>
+
+          <div id="tipDay">
+            "Consejo del dia" Comer 5 frutas o verduras al dia
+          </div>
+          <dl>
+            <dt>Si tomas productos milagro para adelgazar, lo unico que bajara de peso sera tu cartera </dt>
+            <dd>Hay que desconfiar de las pastillas para adelgazar</dd>
+            ...
+          </dl>
+          <h1>Menu 2</h1>
+<a href="http://twitter.com/#search?q=peru" target ="_blank">Twitter Peru</a><br />
+<br>
+<br>
+        </div>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
