3701cce2d832cb3dc391b2efa5b8ab76cf7eb1e4
carpediem16 <40045806+carpediem16@users.noreply.github.com>
2019-06-15 06:39:40 -0500

    Update saldo_totalpepe4on.php

--- saldo_totalpepe4on.php
+++ saldo_totalpepe4on.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "sql301.epizy.com";
 $username = "epiz_24023342";
-$password = "n0qwY4JPQ07";
+$password = "n0qwYQ07";
 $dbname = "epiz_24023342_admin";
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
