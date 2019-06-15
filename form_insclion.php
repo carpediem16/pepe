946e9518dd1c22270cb191a0a8a8ffc84ca897b3
carpediem16 <40045806+carpediem16@users.noreply.github.com>
2019-06-15 07:14:03 -0500

    Add files via upload

---
+++ form_insclion.php
@@ -0,0 +1,29 @@
+<HTML>
+<HEAD><TITLE>Insertar compras</TITLE>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<STYLE>
+ BODY {
+ scrollbar-face-color:maroon;
+scrollbar-highlight-color:green; scrollbar-3dlight-color:blue;
+scrollbar-darkshadow-color:black; scrollbar-shadow-color:white;
+scrollbar-arrow-color:white; scrollbar-track-color:black;     
+color : yellow;
+font-size : 12pt;}
+</STYLE></HEAD>
+<font color="lime" size="5">* * * Insertar ingresos* * *</font>
+<BODY BGCOLOR=black>
+<form method="post" action="insertaclion.php"> 
+<h1>
+dni  :<input type="text" name="dni" size="10" maxlength="10" style="background: navy;border:1px solid gold;color:white;font-weight:bold;"></h1> 
+<h1>
+nombre  :<input type="text" name="name" size="10" maxlength="10" style="background: navy;border:1px solid gold;color:white;font-weight:bold;"></h1> 
+<h1>
+habitacion :<input type="text" name="habitacion" size="10" maxlength="10" style="background: navy;border:1px solid gold;color:white;font-weight:bold;"></h1> 
+<h1>
+monto :<input type="text" name="monto" size="10" maxlength="10" style="background: navy;border:1px solid gold;color:white;font-weight:bold;"></h1><h1>
+local :<input type="text" name="local" size="10" maxlength="10" style="background: navy;border:1px solid gold;color:white;font-weight:bold;"></h1><br> 
+<input type="submit" value="Enviar datos" style="background: maroon;border: 1px solid lime;color:white;font-weight:bold;"> 
+<input type="Reset" value="Borrar todo" style="background: maroon;border:1px solid lime;color:white;font-weight:bold;"> 
+</form>
+</BODY>
+</HTML> 
---
+++ form_insgastopepe.php
@@ -0,0 +1,42 @@
+<HTML>
+<HEAD><TITLE>Insertar compras</TITLE>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<STYLE>
+ BODY {
+ scrollbar-face-color:maroon;
+scrollbar-highlight-color:green; scrollbar-3dlight-color:blue;
+scrollbar-darkshadow-color:black; scrollbar-shadow-color:white;
+scrollbar-arrow-color:white; scrollbar-track-color:black;     
+color : yellow;
+font-size : 12pt;}
+</STYLE></HEAD>
+<font color="lime" size="5">* * * Insertar gastos* * *</font>
+<BODY BGCOLOR=black>
+<form method="post" action="insertagasppcon.php"> 
+<h1>
+codigo  :<input type="text" name="dni" size="10" maxlength="10" style="background: navy;border:1px solid gold;color:white;font-weight:bold;"></h1> 
+<h1>
+nombre  :<input type="text" name="name" size="10" maxlength="10" style="background: navy;border:1px solid gold;color:white;font-weight:bold;"></h1> 
+<h1>
+habitacion :<input type="text" name="habitacion" size="10" maxlength="10" style="background: navy;border:1px solid gold;color:white;font-weight:bold;"></h1> 
+<h1>
+monto :<input type="text" name="monto" size="10" maxlength="10" style="background: navy;border:1px solid gold;color:white;font-weight:bold;"></h1><h1>
+local :<input type="text" name="local" size="10" maxlength="10" style="background: navy;border:1px solid gold;color:white;font-weight:bold;"></h1><br> 
+<input type="submit" value="Enviar datos" style="background: maroon;border: 1px solid lime;color:white;font-weight:bold;"> 
+<input type="Reset" value="Borrar todo" style="background: maroon;border:1px solid lime;color:white;font-weight:bold;"> 
+</form>
+portero c001
+limpieza c002
+lavado c003
+materiales cuartos c004
+<br>
+agua c005
+luz c006
+licores c007
+diversos c008
+cerveza c009
+<br>
+gaseosas c010
+
+</BODY>
+</HTML> 
